17ea37fbc00999c9fb052d9210fb576f8586985a
cihuyyama <miqbalalhabib@gmail.com>
2023-08-26 17:02:58 +0700

    adding billboard and randome movie endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Billboard from '@/components/Billboard'
+import Navbar from '@/components/Navbar'
 import useCurrentUser from '@/hooks/useCurrentUser'
 import { NextPageContext } from 'next'
 import { getSession, signOut } from 'next-auth/react'
@@ -20,19 +22,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
-
 
 
   return (
     <>
-      <h1 className="text-2xl">Netflix Clone</h1>
-      <p className='text-white'>
-        Logged in as : {user?.name}
-      </p>
-      <button className='h-10 w-full bg-black' onClick={() => signOut()}>
-        Logout
-      </button>
+      <Navbar/>
+      <Billboard/>
     </>
   )
 }
