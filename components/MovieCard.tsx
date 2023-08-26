ec79296a18ae161a2c99a51501c58ffd5449f0c4
cihuyyama <miqbalalhabib@gmail.com>
2023-08-27 03:43:21 +0700

    adding info modal

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { BsFillPlayFill } from 'react-icons/bs'
 import FavoriteButton from './FavoriteButton'
 import { useRouter } from 'next/router'
+import useInfoModalStore from '@/hooks/useInfoModalStore'
+import { BiChevronDown } from "react-icons/bi";
+
 
 interface MovieCardProps {
     data: Record<string, any>
@@ -12,6 +15,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
     const router = useRouter()
+    const { openModal } = useInfoModalStore()
 
     return (
         <div className='group bg-zinc-900 col-span-1 relative h-[12vw]'>
@@ -27,10 +31,15 @@ const MovieCard: React.FC<MovieCardProps> = ({
                     <div className='flex flex-row items-center gap-3'>
                         <div 
                         onClick={()=>router.push(`/watch/${data?.id}`)}
-                        className='cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-black rounded-full flex justify-center items-center transition hover:bg-neutral-300'>
-                            <BsFillPlayFill/>
+                        className='cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300'>
+                            <BsFillPlayFill className='text-black'/>
                         </div>
                         <FavoriteButton movieId={data?.id}/>
+                        <div
+                        onClick={()=> openModal(data?.id)}
+                        className='cursor-pointer ml-auto group/item w-6 h-6 lg:h-10 lg:w-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'>
+                            <BiChevronDown size={30} className='text-white group-hover/item:text-neutral-300' />
+                        </div>
                     </div>
 
                     <p className='text-green-400 font-semibold mt-4'>
--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -13,7 +13,7 @@ const PlayButton: React.FC<PlayButtonProps> = ({movieId}) => {
     return (
         <div 
         onClick={() => router.push(`/watch/${movieId}`)}
-        className='cursor-pointer bg-black rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-neutral-300 transition'>
+        className='text-black cursor-pointer bg-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-neutral-300 transition'>
             <BsFillPlayFill size={23} className='mr-1' />
             Play
         </div>
